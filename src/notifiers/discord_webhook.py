061dfd3fbcce0a05294c2208735c56c4fc9899f5
Marc Sances <marcsances@gmail.com>
2022-02-27 00:19:29 +0100

    Notify outages through specific webhooks

--- src/notifiers/discord_webhook.py
+++ src/notifiers/discord_webhook.py
@@ -15,6 +15,32 @@ class DiscordWebhook(OutageLevelObserver, QuantitativeObserver):
     def notify_outage(self, value: OutageLevel, url: str):
         if value != self.last_outage:
             self.last_outage = value
+            payload = {
+                "content": None,
+                "embeds": [
+                    {
+                        "title": "Estado del servidor",
+                        "color": self.__get_color(self.last_outage),
+                        "fields": [
+                            {
+                                "name": "URL",
+                                "value": url,
+                                "inline": True
+                            },
+                            {
+                                "name": "Estado",
+                                "value": self.__get_status(self.last_outage),
+                                "inline": True
+                            }
+                        ]
+                    }
+                ]
+            }
+            for webhook in STATUS_WEBHOOKS:
+                print("POST {}\n".format(webhook, json.dumps(payload)))
+                if not DRY_MODE:
+                    requests.post(webhook, data=json.dumps(payload),
+                                  headers={"Content-Type": "application/json;charset=UTF-8"})
 
     def __get_color(self, outage: OutageLevel) -> int:
         hx = "00ff00"
@@ -63,7 +89,7 @@ class DiscordWebhook(OutageLevelObserver, QuantitativeObserver):
             }
           ]
         }
-        for webhook in DISCORD_WEBHOOKS:
+        for webhook in PING_WEBHOOKS:
             print("POST {}\n".format(webhook, json.dumps(payload)))
             if not DRY_MODE:
                 requests.post(webhook, data=json.dumps(payload), headers={"Content-Type": "application/json;charset=UTF-8"})
